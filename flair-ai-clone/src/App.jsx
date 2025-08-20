1111c4896819ed59ad525b124899d03be37fe8d4
chayy7 <chaitanyasrinu4735@gmail.com>
2025-08-21 04:39:20 +0530

    Assets

--- flair-ai-clone/src/App.jsx
+++ flair-ai-clone/src/App.jsx
@@ -2,8 +2,11 @@ import { useState } from 'react'
 import './App.css'
 import NavBar from './Components/NavBar'
 import Hero from './Components/HeroSection'
+import Screen from './Components/screen'
 import Carousel from './Components/Carousel'
 import MarketingSection from './Components/MarketingSection'
+import StageScene from './Components/StageScene'
+import VideoContentSection from './Components/VideoContentSection'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -11,8 +14,11 @@ function App() {
     <>
     <NavBar/>
     <Hero />
+    <Screen />
     <Carousel />
     <MarketingSection />
+    <StageScene />
+    <VideoContentSection />
     </>
   )
 }
